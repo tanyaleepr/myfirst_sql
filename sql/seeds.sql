d01580e5fb01dcc0ae09d5c0a7b3a033a650652d
tanyaleepr <tanyaleepr@gmail.com>
2022-03-03 23:18:41 -0600

    added employee info in seeds

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -10,31 +10,30 @@ INSERT INTO department (name)
 VALUES ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 100000, 1);
+VALUES ("Sales Lead", 50000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 150000, 2);
+VALUES ("Lead Engineer", 100000, 2);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Software Engineer", 120000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 125000, 3);
+VALUES ("Accountant", 75000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Team Lead", 250000, 4);
+VALUES ("Legal Team Lead", 110000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mike", "Chan", 2, 1);
+VALUES ("Juan", "El Bobo", 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ashley", "Rodriguez", 3, null);
+VALUES ("Josue", "Rodriguez", 3, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kevin", "Tupik", 4, 3);
+VALUES ("Xi", "Hua", 4, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Malia", "Brown", 5, null);
+VALUES ("Jennifer", "Putin", 5, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sarah", "Lourd", 2, null);
+VALUES ("Tanya", "Gonzalez", 2, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Allen", 4, 7);
+VALUES ("Tom", "AndJerry", 4, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Christian", "Eckenrode", 1, 2);
+VALUES ("Christian", "Castro", 1, 2);
 
-© 2022 GitHub, Inc.
