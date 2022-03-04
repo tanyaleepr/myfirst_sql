0c8de05a2b141c77425b71cedf2f17d000c3aa38
tanyaleepr <tanyaleepr@gmail.com>
2022-03-03 22:32:52 -0600

    added the function to delete employee info

--- server.js
+++ server.js
@@ -230,3 +230,55 @@ function viewEmployeeByDepartment() {
           });
       });
   }
+
+  
+//"Remove Employees" / DELETE, DELETE FROM
+// Make a employee array to delete
+function removeEmployees() {
+    console.log("Deleting an employee");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name
+        FROM employee e`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const deleteEmployeeChoices = res.map(({ id, first_name, last_name }) => ({
+        value: id, name: `${id} ${first_name} ${last_name}`
+      }));
+  
+      console.table(res);
+      console.log("ArrayToDelete!\n");
+  
+      promptDelete(deleteEmployeeChoices);
+    });
+  }
+  
+  // User choose the employee list, then employee is deleted
+  function promptDelete(deleteEmployeeChoices) {
+  
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employeeId",
+          message: "Which employee do you want to remove?",
+          choices: deleteEmployeeChoices
+        }
+      ])
+      .then(function (answer) {
+  
+        var query = `DELETE FROM employee WHERE ?`;
+        // when finished prompting, insert a new item into the db with that info
+        connection.query(query, { id: answer.employeeId }, function (err, res) {
+          if (err) throw err;
+  
+          console.table(res);
+          console.log(res.affectedRows + "Deleted!\n");
+  
+          firstPrompt();
+        });
+      });
+  }
+  
