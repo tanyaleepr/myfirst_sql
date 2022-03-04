183ac9f0cc308c7df0f54e7fd649fea04f135e55
tanyaleepr <tanyaleepr@gmail.com>
2022-03-03 22:33:36 -0600

    added function to update employee info

--- server.js
+++ server.js
@@ -282,3 +282,58 @@ function removeEmployees() {
       });
   }
   
+//"Update Employee Role" / UPDATE,
+function updateEmployeeRole() { 
+    employeeArray();
+  
+  }
+  
+  function employeeArray() {
+    console.log("Updating an employee");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    JOIN role r
+      ON e.role_id = r.id
+    JOIN department d
+    ON d.id = r.department_id
+    JOIN employee m
+      ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const employeeChoices = res.map(({ id, first_name, last_name }) => ({
+        value: id, name: `${first_name} ${last_name}`      
+      }));
+  
+      console.table(res);
+      console.log("employeeArray To Update!\n")
+  
+      roleArray(employeeChoices);
+    });
+  }
+  
+  function roleArray(employeeChoices) {
+    console.log("Updating an role");
+  
+    var query =
+      `SELECT r.id, r.title, r.salary 
+    FROM role r`
+    let roleChoices;
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      roleChoices = res.map(({ id, title, salary }) => ({
+        value: id, title: `${title}`, salary: `${salary}`      
+      }));
+  
+      console.table(res);
+      console.log("roleArray to Update!\n")
+  
+      promptEmployeeRole(employeeChoices, roleChoices);
+    });
+  }
+  
