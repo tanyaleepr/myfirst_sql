636f098a8bc6a68beeec7d83a626c97394938e32
tanyaleepr <tanyaleepr@gmail.com>
2022-03-03 23:47:42 -0600

    schema info for drop and create tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,54 @@
-DROP DATABASE IF EXISTS employeesDB;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
-CREATE DATABASE employeesDB;
-
-USE employeesDB;
-
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) NULL,
-  PRIMARY KEY (id)
+CREATE TABLE departments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(45) NULL,
-  salary DECIMAL(10.3) NULL,
-  department_id INT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL UNIQUE,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(45) NULL,
-  last_name VARCHAR(45) NULL,
-  role_id INT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
-);
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+
+-- DROP DATABASE IF EXISTS employeesDB;
+
+-- CREATE DATABASE employeesDB;
+
+-- USE employeesDB;
+
+-- CREATE TABLE department (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   name VARCHAR(45) NULL,
+--   PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE role (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   title VARCHAR(45) NULL,
+--   salary DECIMAL(10.3) NULL,
+--   department_id INT NULL,
+--   PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE employee (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   first_name VARCHAR(45) NULL,
+--   last_name VARCHAR(45) NULL,
+--   role_id INT NULL,
+--   manager_id INT NULL,
+--   PRIMARY KEY (id)
+-- );
